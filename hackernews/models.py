28ef16a80821c9fd7f44eed366de80a2f8ab5575
garg-anant <ag270@snu.edu.in>
2019-04-12 13:31:07 +0530

    scraped the home page data considering all the discrepencies and added data to the database

--- hackernews/models.py
+++ hackernews/models.py
@@ -11,23 +11,23 @@ class ProfileUser(AbstractUser):
 	# REQUIRED_FIELDS = ['username']
 
 	def __str__(self):
-		return self.email
+		return self.username
 
 class NewsLinks(models.Model):
-	title = models.CharField(max_length=300)
+	title = models.CharField(max_length=300, null=True, blank=True)
 	posted_by = models.ForeignKey(ProfileUser, on_delete=models.CASCADE, null=True, blank=True)
-	title_link = models.URLField(max_length=400)
+	title_link = models.URLField(max_length=400, null=True, blank=True)
 	num_comments = models.PositiveIntegerField(default=0, null=True, blank=True)
 	hackernews_post_id = models.PositiveIntegerField(unique=True)
 	# karma_points = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
-	karma_points = models.CharField(max_length=10, null=True, blank=True)
+	karma_points = models.CharField(max_length=15, null=True, blank=True)
 	time_posted = models.DateTimeField(null=True, blank=True)
 
 	upvotes = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
 	downvotes = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
 
 	def __str__(self):
-		return self.title
+		return '%s - ' % (self.hackernews_post_id)
 
 class comments(models.Model):
 	newslink = models.ForeignKey(NewsLinks, on_delete=models.CASCADE)
--- hackernews/views.py
+++ hackernews/views.py
@@ -11,7 +11,7 @@ import datetime
 
 # from .tasks import test, return_5, myfunc
 from .tasks import myfunc
-from .models import NewsLinks
+from .models import NewsLinks, ProfileUser
 
 
 # Create your views here.
@@ -41,58 +41,95 @@ def home(request):
 		news_element = soup.find_all("tr", {'class':'athing'})
 
 		for element in news_element:
-
-
 			#get-hn-id - completed
-			
 			soup_hnnews_id = BeautifulSoup(str(element),'html.parser')
 			hnnews_id = soup_hnnews_id.find("tr",{'class':'athing'})
 			hnnews_id = hnnews_id.get('id')
 			print(hnnews_id)
 			
-			#top-line - completed
-			'''	
-			soup_top_line = BeautifulSoup(str(element),'html.parser')
-			news_link = soup_top_line.find("a", {'class':'storylink'})
-			print(news_link.text)
-			print(news_link.get('href'))
-			print(urlparse(news_link.get('href')).netloc)
-			print('\n')
-			'''
-			#bottom-line - completed
-			next_row = element.findNext('tr')
-			soup_bottom_link = BeautifulSoup(str(next_row),'html.parser')
+			#ADD USERS ONLY IF NOT ALREADY EXISTS
+			#ADD Newslinks and all related data if not already exists
+			news_items = NewsLinks.objects.filter(hackernews_post_id=int(hnnews_id)).exists()
+			if news_items == False:
+				print('news does not exists..')
+				news_add,_ = NewsLinks.objects.get_or_create(hackernews_post_id=int(hnnews_id))
 
-			karma_point = soup_bottom_link.find("span",{'class':'score'})
-			post_by = soup_bottom_link.find("a",{'class':'hnuser'})
-			time_of_upload = soup_bottom_link.find("span",{'class':'age'})
+				add_news_fields = NewsLinks.objects.get(hackernews_post_id=int(hnnews_id))
+				#top-line - completed
+				soup_top_line = BeautifulSoup(str(element),'html.parser')
+				news_link = soup_top_line.find("a", {'class':'storylink'})
+				# print(news_link.text)
+				add_news_fields.title = news_link.text #storing title
+				# print(news_link.get('href'))
+				add_news_fields.title_link = news_link.get('href') #storing title link
+				# print(urlparse(news_link.get('href')).netloc)
+				# print('\n')
 
-			comments = soup_bottom_link.find_all('a',{'href':'item?id='+hnnews_id})
-			if len(comments) == 2:
-				if "discuss" in comments[1].text:
-					print('0') # 0 comments in case of "discuss" 
+
+				#bottom-line - completed
+				next_row = element.findNext('tr') #Getting the soup element of bottom row
+				soup_bottom_link = BeautifulSoup(str(next_row),'html.parser')
+
+				karma_point = soup_bottom_link.find("span",{'class':'score'})
+				post_by = soup_bottom_link.find("a",{'class':'hnuser'})
+				time_of_upload = soup_bottom_link.find("span",{'class':'age'})
+
+				#check if the user is  displayed on page and accordingly register his/her profile
+				users = soup_bottom_link.find("a",{'class':'hnuser'})
+				if users is not None:
+					#check and register if displayed user already exists
+					profile_user = ProfileUser.objects.filter(username=post_by.text).exists()
+					if profile_user == False:
+						print('user does not exist..')
+						new_hnuser,_ = ProfileUser.objects.get_or_create(username=post_by.text)
+						print('user registered')
+					else:
+						print('user exists')
+						pass
 				else:
-					print(comments[1].text.split('\xa0')[0]) #number of comments
-			else:
-				print(None) #number of comments stored as None when unavailable 
+					print('User is not displayed on the home page and therefore no link present')
+				#Check and add comment only if it is diplayed on the page
+				comments = soup_bottom_link.find_all('a',{'href':'item?id='+hnnews_id})
+				if len(comments) == 2:
+					if "discuss" in comments[1].text:
+						print('0') # 0 comments in case of "discuss" 
+					else:
+						print(comments[1].text.split('\xa0')[0]) #number of comments
+						#Getting onto the comments page
+						comments_link = comments[0].get('href')
+						comments_page_req = requests.get("https://news.ycombinator.com/"+comments_link)
+						print(comments_page_req) #to check if comments page is loading or not
 
-			#storing time
-			if "hour" in time_of_upload.text:
-				print(datetime.datetime.now() - datetime.timedelta(hours=int(time_of_upload.text.split(' ')[0])))
-			if "minute" in time_of_upload.text:
-				print(datetime.datetime.now() - datetime.timedelta(minutes=int(time_of_upload.text.split(' ')[0])))
+						comments_soup = BeautifulSoup(comments_page_req.text,'html.parser')
 
-			'''
-			print(soup_bottom_link)
-			print(karma_point.text)
-			print(post_by.text)
-			print(time_of_upload.text)
-			print('\n')
-			'''
 
-		
-		ctx = {
-			# 'titles' : titles,
-			# 'list_news_links' : list_news_links,
-		}
+				else:
+					print(None) #number of comments stored as None when unavailable 
+
+				#storing time
+				if "hour" in time_of_upload.text:
+					posted_time = datetime.datetime.now() - datetime.timedelta(hours=int(time_of_upload.text.split(' ')[0]))
+				if "minute" in time_of_upload.text:
+					posted_time = datetime.datetime.now() - datetime.timedelta(minutes=int(time_of_upload.text.split(' ')[0]))
+
+				
+				# print(soup_bottom_link)
+
+				karma_points_check = soup_bottom_link.find('span',{'class':'score'})
+				if karma_points_check is not None:
+					#check and add data in karma_points if displayed user's karma points already exists
+					# print(karma_point.text)
+					add_news_fields.karma_points = karma_point.text #storing karma points
+				
+				print(post_by.text)
+				print(time_of_upload.text)
+				add_news_fields.time_posted = posted_time #storing time of post as calculated above
+				print('\n')
+
+				add_news_fields.save()
+				
+
+
+	
+	ctx = {}
 	return render(request, 'hackernews/home.jinja', ctx)
